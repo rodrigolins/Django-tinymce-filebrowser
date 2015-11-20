d78fd033e69ee84fa831d4baa64638e0a4504f71
Rodrigo Lins <rodrigo.lins@exelonix.com>
2015-11-20 13:22:15 +0100

    Fix admin.py error

--- mce_filebrowser/admin.py
+++ mce_filebrowser/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 
 class MCEFilebrowserAdmin(admin.ModelAdmin):
-  
-  class Media:
-      js = ('mce_filebrowser/js/filebrowser_init.js',)
+
+    class Media:
+        js = ('mce_filebrowser/js/filebrowser_init.js',)
